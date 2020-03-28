320136acc9b24749d49c94f696d88f2ed3dfb253
damansahi <dbsahi@gmail.com>
2020-03-28 18:12:40 -0400

    Home page map

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
+import MapView, { PROVIDER_GOOGLE, Circle } from 'react-native-maps';
+import { Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import { customMapStyle } from '../components/maps/MapStyles';
 
 const Example = styled.View`
-    height: 300;
+    height: 300px;
     background-color: red;
 `;
 
 const TextExample = styled.Text`
-    font-size: 30;
+    font-size: 18;
 `;
 
 function HomeScreen() {
     return (
         <Example>
-            <TextExample>Home</TextExample>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.mapStyle}
+                customMapStyle={customMapStyle}
+                showsUserLocation={true}
+            ></MapView>
+            <TouchableOpacity style={styles.mapBtn}>
+                <TextExample>View Map</TextExample>
+            </TouchableOpacity>
         </Example>
     );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: 300
+    },
+    mapBtn: {
+        position: 'absolute',
+        top: 60,
+        right: 20,
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+        padding: 8,
+        width: 120,
+        borderRadius: 12,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    }
+});
