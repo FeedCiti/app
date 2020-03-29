02efcbd2e3fdfa05d0b0957a2ed67d7c3ef4c5d8
alexbarksdale <ale.barksdale@gmail.com>
2020-03-29 00:44:05 -0500

    [Add] Fonts

--- src/components/maps/CitiMapStyles.js
+++ src/components/maps/CitiMapStyles.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import {View} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import { View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import styled from 'styled-components/native';
-import {customMapStyle} from './MapStyles';
+import { customMapStyle } from './MapStyles';
 
 const MapStyles = styled(MapView)`
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 
 function CitiMapStyles() {
-  return (
-    <View />
-    /*<MapStyles
+    return (
+        <View />
+        /*<MapStyles
             provider={PROVIDER_GOOGLE}
             customMapStyle={customMapStyle}
             showsUserLocation={true}
         />*/
-  );
+    );
 }
 
 export default CitiMapStyles;
--- src/components/navigation/CitiStack.js
+++ src/components/navigation/CitiStack.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {useNavigation} from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
 import Feather from 'react-native-vector-icons/Feather';
 import styled from 'styled-components/native';
+import Fonts from '../../../config/Fonts';
 
 import MapScreen from '../../screens/MapScreen';
 import GivingScreen from '../../screens/GivingScreen';
@@ -11,53 +12,57 @@ import GivingScreen from '../../screens/GivingScreen';
 const Stack = createStackNavigator();
 
 const Test = styled.Text`
-  font-size: 18px;
-  margin-right: 12px;
-  color: ${props => props.theme.primary};
+    font-size: 18px;
+    margin-right: 12px;
+    font-family: ${Fonts.F700};
+    color: ${(props) => props.theme.primary};
 `;
 
 const BackBtn = styled(Feather)`
-  font-size: 22px;
-  margin-left: 12px;
-  color: ${props => props.theme.gray800};
+    font-size: 22px;
+    margin-left: 12px;
+    color: ${(props) => props.theme.gray800};
 `;
 
-export function MapStack() {
-  const navigation = useNavigation();
+function MapStack() {
+    const navigation = useNavigation();
 
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Map"
-        component={MapScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="GiveInput"
-        component={GivingScreen}
-        options={{
-          title: null,
-          headerLeft: () => {
-            return (
-              <TouchableOpacity
-                onPress={() => {
-                  navigation.goBack();
-                }}>
-                <BackBtn name="chevron-left" />
-              </TouchableOpacity>
-            );
-          },
-          headerRight: () => {
-            return (
-              <TouchableOpacity>
-                <Test>Send</Test>
-              </TouchableOpacity>
-            );
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Map'
+                component={MapScreen}
+                options={{
+                    headerShown: false
+                }}
+            />
+            <Stack.Screen
+                name='GiveInput'
+                component={GivingScreen}
+                options={{
+                    title: null,
+                    headerLeft: () => {
+                        return (
+                            <TouchableOpacity
+                                onPress={() => {
+                                    navigation.goBack();
+                                }}
+                            >
+                                <BackBtn name='chevron-left' />
+                            </TouchableOpacity>
+                        );
+                    },
+                    headerRight: () => {
+                        return (
+                            <TouchableOpacity>
+                                <Test>Send</Test>
+                            </TouchableOpacity>
+                        );
+                    }
+                }}
+            />
+        </Stack.Navigator>
+    );
 }
+
+export default MapStack;
