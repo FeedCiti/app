9860d66b82f3840c9796025112b71a3aec77dc37
alexbarksdale <ale.barksdale@gmail.com>
2020-03-29 12:06:39 -0500

    Resolved merge conflicts

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-=======
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
->>>>>>> native
+    presets: ['module:metro-react-native-babel-preset']
 };
--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -10,4 +10,3 @@ function MapScreen() {
 }
 
 export default MapScreen;
-
